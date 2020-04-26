68f906363f0953df701b9fbb86bb70c5d55f5295
mhafet17 <do.crazy192@gmail.com>
2020-04-26 10:18:28 +0700

    menambah model member

---
+++ models/Member.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const MemberSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+
+
+});
+
+module.exports = mongoose.model('Member', MemberSchema)
