1ccef2806c29d83e0f6c05ed7cd1d3a66fb81e50
mhafet17 <do.crazy192@gmail.com>
2020-05-11 10:30:55 +0700

    modifikasi admin router

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const adminController = require('../controller/adminController');
+const {
+    uploadSingle,
+    uploadMultiple
+} = require('../middleware/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
 
@@ -7,15 +11,23 @@ router.get('/dashboard', adminController.viewDashboard);
 router.get('/category', adminController.viewCategory);
 router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
 
 // Route Bank
 router.get('/bank', adminController.viewBank);
+router.post('/bank', uploadSingle, adminController.addBank);
+router.put('/bank', uploadSingle, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
 
 // Route Item
 router.get('/item', adminController.viewItem);
+router.post('/item', uploadMultiple, adminController.addItem);
+router.get('/item/show-image/:id', adminController.showImageItem);
+router.get('/item/:id', adminController.showEditItem);
+router.put('/item/:id', uploadMultiple, adminController.editItem);
+router.delete('/item/:id/delete', adminController.deleteItem);
 
-// Route Booking
+// Route Booking 
 router.get('/booking', adminController.viewBooking);
-
 
 module.exports = router;
