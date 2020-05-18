bc3855ca162e4de444e65f317ba2a607cb7bd263
mhafet17 <do.crazy192@gmail.com>
2020-05-18 19:53:44 +0700

    menambah api controller

---
+++ controller/apiController.js
@@ -0,0 +1,8 @@
+const Item = require('../models/Item');
+const Treasure = require('../models/Activity');
+const Treveler = require('../models/Booking');
+const Category = require('../models/Category');
+const Bank = require('../models/Bank');
+const Booking = require('../models/Booking');
+const Member = require('../models/Member');
+
