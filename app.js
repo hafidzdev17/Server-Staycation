e0f514b53cf5aaace5c69fa9d0ed5b04838f23c9
mhafet17 <do.crazy192@gmail.com>
2020-05-21 09:42:54 +0700

    menambah config api routes

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var usersRouter = require('./routes/users');
 
 // Router Admin
 const adminRouter = require('./routes/admin');
+const apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -50,7 +51,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // admin
+// admin
 app.use('/admin', adminRouter);
+app.use('/api/v1/member', apiRouter);
 
 
 // catch 404 and forward to error handler
